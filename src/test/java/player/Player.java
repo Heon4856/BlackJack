7af609b5d5d0c61c5f73c51b9cdd67f792692910
Sungheon Jeong <elon4856@gmail.com>
2021-08-01 00:13:36 +0900

    feat: Player GetCard tdd

--- src/test/java/player/Player.java
+++ src/test/java/player/Player.java
@@ -1,9 +1,23 @@
 package test.java.player;
 
+import java.util.ArrayList;
+
 public class Player {
     private static int PlayerMoney =1000;
+    private static ArrayList<String> PlayerCard= new ArrayList<>();
+
 
     public static int getMoney(int BatMoney){
+
         return BatMoney + Player.PlayerMoney;
     }
+
+
+    public static ArrayList<String> GetCard(String card){
+        Player.PlayerCard.add("s1");
+        System.out.println(PlayerCard);
+        return Player.PlayerCard;
+
+    }
+
 }
--- src/test/java/player/PlayerTest.java
+++ src/test/java/player/PlayerTest.java
@@ -2,6 +2,8 @@ package test.java.player;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PlayerTest {
@@ -10,8 +12,14 @@ public class PlayerTest {
     void GetMoney(){
         int result = Player.getMoney(100);
         assertEquals(1100, result);
+    }
 
-
+    @Test
+    void GetCard(){
+        ArrayList<String> result = Player.GetCard("s1");
+        ArrayList<String> test = new ArrayList();
+        test.add("s1");
+        assertEquals( test, result);
     }
 
     }
