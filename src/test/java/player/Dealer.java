5d43db6d114f41ad498c5baad945484a3eb2c041
Sungheon Jeong <elon4856@gmail.com>
2021-08-04 23:45:41 +0900

    feat: dealer에게 적용되는 룰잉 16이하면 카드를 받는 메소드 만듬

--- src/test/java/player/Dealer.java
+++ src/test/java/player/Dealer.java
@@ -1,4 +1,11 @@
 package test.java.player;
 
-public class Dealer {
+public class Dealer extends Player{
+
+    public void under16_getcards_else_stay(int sum_cards){
+        if (sum_cards <=16){
+            Dealer.GetCard();
+        }
+
+    }
 }
